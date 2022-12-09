570820c930c664cac8ecc6c19fd671ddaf36005c
dawidhallmann <daw-hal@wp.pl>
2022-12-09 14:50:26 +0100

    override visit && handle app errors && change config

--- integration/cypress.config.js
+++ integration/cypress.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 8000,
+  requestTimeout: 20000,
+  watchForFileChanges: false,
   e2e: {
+    baseUrl: 'http://localhost:8080/',
     specPattern: "cypress/specs/**/*.cy.{js,jsx,ts,tsx}",
     experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
--- integration/cypress/support/commands.js
+++ integration/cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.overwrite("visit", (originalFn, url, options) => {
+    const { baseUrl } = Cypress.config();
+    return originalFn(`${baseUrl}${url || ""}`, {
+      ...(options || {}),
+    });
+});
+  
+Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+});
